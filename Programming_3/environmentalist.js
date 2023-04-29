70270241638fd2de058ff51582a96301563e16a6
Համլետ <130160470+HamletAraqelyan29@users.noreply.github.com>
2023-04-29 17:57:15 +0400

    Add files via upload

---
+++ Programming_3/environmentalist.js
@@ -0,0 +1,140 @@
+let LivingCreature = require("./LivingCreature")
+module.exports = class Environmentalist extends LivingCreature {
+    constructor(x, y) {
+        super(x, y)
+        this.energy = 17
+        this.directions = [];
+    }
+
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+
+
+    chooseCell(char, char2) {
+        this.getNewCoordinates()
+        let found = []
+
+
+        for (let i in this.directions) {
+            let x = this.directions[i][0]
+            let y = this.directions[i][1]
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == char || matrix[y][x] == char2) {
+                    found.push(this.directions[i])
+                }
+            }
+        }
+
+
+        return found
+
+    }
+
+
+    mul() {
+        let emptyCell = this.chooseCell(0)
+        let newCell = emptyCell[Math.floor(Math.random() * emptyCell.length)]
+        if (newCell) {
+            let newX = newCell[0]
+            let newY = newCell[1]
+
+            matrix[newY][newX] = 4
+
+            let envEat = new Environmentalist(newX, newY)
+
+            environmentalistArr.push(envEat)
+
+
+        }
+    }
+
+
+    eat() {
+        let emptyCell = this.chooseCell(2,)
+        let newCell = emptyCell[Math.floor(Math.random() * emptyCell.length)]
+
+        if (newCell) {
+            this.energy += 5
+            let newX = newCell[0]
+            let newY = newCell[1]
+
+            for (let i in predatorArr) {
+                if (newX == predatorArr[i].x && newY == predatorArr[i].y) {
+                    predatorArr.splice(i, 1)
+                }
+            }
+            for (let i in grassEaterArr) {
+                if (newX == grassEaterArr[i].x && newY == grassEaterArr[i].y) {
+                    grassEaterArr.splice(i, 1)
+                }
+            }
+            for (let i in mineArr) {
+                if (newX == mineArr[i].x && newY == mineArr[i].y) {
+                    mineArr.splice(i, 1)
+                }
+            }
+
+            matrix[newY][newX] = 4
+            matrix[this.y][this.x] = 0
+
+
+            this.x = newX
+            this.y = newY
+
+            if (this.energy > 40) {
+                this.mul()
+            }
+
+        } else {
+            this.move()
+        }
+    }
+
+    move() {
+        let emptyCell = this.chooseCell(0)
+        let newCell = emptyCell[Math.floor(Math.random() * emptyCell.length)]
+
+        if (newCell) {
+            let newX = newCell[0]
+            let newY = newCell[1]
+
+            matrix[newY][newX] = 4
+            matrix[this.y][this.x] = 0
+
+            this.x = newX
+            this.y = newY
+
+            this.energy--
+
+            if (this.energy < 0) {
+                this.die()
+            }
+        }
+    }
+
+
+    die() {
+        matrix[this.y][this.x] = 0
+
+        for (let i in environmentalistArr) {
+            if (this.x == environmentalistArr[i].x && this.y == environmentalistArr[i].y) {
+                environmentalistArr.splice(i, 1)
+            }
+        }
+    }
+
+
+
+}
---
+++ Programming_3/server.js
@@ -0,0 +1,274 @@
+
+const { count } = require("console")
+var express = require("express")
+var app = express()
+var server = require("http").Server(app)
+var io = require("socket.io")(server)
+var fs = require("fs")
+const Aboriginal = require("./aboriginal")
+const Environmentalist = require("./environmentalist")
+const Grass = require("./grass")
+const GrassEater = require("./grassEater")
+const Hunter = require("./hunter")
+const Mine = require("./Mine")
+const Predator = require("./predator")
+
+
+app.use(express.static("."))
+
+app.get("/", function (req, res) {
+    res.redirect("home.html")
+})
+
+server.listen(3001)
+
+
+
+
+function matrixGenerator(matrixSize, grass, grassEater, predator, environmentalist, hunter, mine, aboriginal) {
+    var matrix = []
+    for (let i = 0; i < matrixSize; i++) {
+        matrix.push([])
+        for (let j = 0; j < matrixSize; j++) {
+            matrix[i].push(0)
+        }
+    }
+
+
+    for (let i = 0; i < grass; i++) {
+        let x = Math.floor(Math.random() * matrixSize)
+        let y = Math.floor(Math.random() * matrixSize)
+        matrix[y][x] = 1
+    }
+
+
+    for (let i = 0; i < grassEater; i++) {
+        let x = Math.floor(Math.random() * matrixSize)
+        let y = Math.floor(Math.random() * matrixSize)
+        matrix[y][x] = 2
+    }
+
+    for (let i = 0; i < predator; i++) {
+        let x = Math.floor(Math.random() * matrixSize)
+        let y = Math.floor(Math.random() * matrixSize)
+        matrix[y][x] = 3
+    }
+    for (let i = 0; i < environmentalist; i++) {
+        let x = Math.floor(Math.random() * matrixSize)
+        let y = Math.floor(Math.random() * matrixSize)
+        matrix[y][x] = 4
+    }
+    for (let i = 0; i < hunter; i++) {
+        let x = Math.floor(Math.random() * matrixSize)
+        let y = Math.floor(Math.random() * matrixSize)
+        matrix[y][x] = 5
+    }
+    for (let i = 0; i < mine; i++) {
+        let x = Math.floor(Math.random() * matrixSize)
+        let y = Math.floor(Math.random() * matrixSize)
+        matrix[y][x] = 6
+    }
+    for (let i = 0; i < aboriginal; i++) {
+        let x = Math.floor(Math.random() * matrixSize)
+        let y = Math.floor(Math.random() * matrixSize)
+        matrix[y][x] = 7
+    }
+    return matrix
+}
+
+
+matrix = matrixGenerator(20, 17, 9, 4, 8, 6, 4, 2)
+io.sockets.emit("send matrix", matrix)
+
+
+grassArr = []
+grassEaterArr = []
+predatorArr = []
+environmentalistArr = []
+hunterArr = []
+mineArr = []
+aboriginalArr = []
+
+// //Grass = require("./grass")
+// GrassEater = require("./grassEater")
+// Predator = require("./predator")
+// Environmentalist = require("./environmentalist")
+// Hunter = require("./hunter")
+// Mine = require("./Mine")
+// Aboriginal = require("./aboriginal")
+
+
+
+function createObject() {
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                let grass = new Grass(x, y)
+
+                grassArr.push(grass)
+
+
+            } else if (matrix[y][x] == 2) {
+                let grEat = new GrassEater(x, y)
+                grassEaterArr.push(grEat)
+            }
+            else if (matrix[y][x] == 3) {
+                let prEat = new Predator(x, y)
+                predatorArr.push(prEat)
+            }
+            else if (matrix[y][x] == 4) {
+                let envEat = new Environmentalist(x, y)
+                environmentalistArr.push(envEat)
+            }
+            else if (matrix[y][x] == 5) {
+                let hunEat = new Hunter(x, y)
+                hunterArr.push(hunEat)
+            }
+
+            else if (matrix[y][x] == 6) {
+                let MinE = new Mine(x, y)
+                mineArr.push(MinE)
+            }
+            else if (matrix[y][x] == 7) {
+                let aboEat = new Aboriginal(x, y)
+                aboriginalArr.push(aboEat)
+            }
+
+        }
+    }
+    io.sockets.emit("send matrix", matrix)
+}
+function game() {
+
+    for (let i in grassArr) {
+        grassArr[i].mul()
+    }
+
+
+    for (let i in grassEaterArr) {
+        grassEaterArr[i].eat()
+    }
+
+    for (let i in predatorArr) {
+
+        predatorArr[i].eat()
+    }
+    for (let i in environmentalistArr) {
+        environmentalistArr[i].eat()
+    }
+    for (let i in hunterArr) {
+        hunterArr[i].eat()
+    }
+    for (let i in aboriginalArr) {
+        aboriginalArr[i].eat()
+    }
+
+    for (let i in mineArr) {
+        mineArr[i].mul()
+    }
+
+    io.sockets.emit("send matrix", matrix)
+
+}
+setInterval(game, 250)
+
+io.on("connection", function () {
+    createObject()
+})
+
+
+io.sockets.emit("send matrix", matrix)
+
+
+function AddCharr(n) {
+
+    let x = Math.floor(Math.random() * matrix[0].length)
+    let y = Math.floor(Math.random() * matrix.length)
+
+    matrix[y][x] = n
+
+    if (n == 1) {
+        var gr = new Grass(x, y)
+        grassArr.push(gr)
+
+    }
+
+    else if (n == 2) {
+        var grEat = new GrassEater(x, y)
+        grassEaterArr.push(grEat)
+    }
+    else if (n == 3) {
+        var Pr = new Predator(x, y)
+        predatorArr.push(Pr)
+    }
+    else if (n == 4) {
+        var envEat = new Environmentalist(x, y)
+        environmentalistArr.push(envEat)
+    }
+    else if (n == 5) {
+        var hunEat = new Hunter(x, y)
+        hunterArr.push(hunEat)
+    }
+
+    else if (n == 7) {
+        var aboEat = new Aboriginal(x, y)
+        aboriginalArr.push(aboEat)
+    }
+    else if (n == 6) {
+
+        var MinE = new Mine(x, y)
+        mineArr.push(MinE)
+
+    }
+    io.sockets.emit("send matrix", matrix)
+}
+io.on('connection', function (socket) {
+    socket.on("send button", AddCharr)
+})
+
+io.sockets.emit("send matrix", matrix)
+var statistics = {}
+
+setInterval(function () {
+
+    statistics.grass = grassArr.length
+    statistics.grassEater = grassEaterArr.length
+    statistics.predator = predatorArr.length
+    statistics.hunter = hunterArr.length
+    statistics.aboriginal = aboriginalArr.length
+    statistics.environmentalist = environmentalistArr.length
+
+    fs.writeFile("statistics.json", JSON.stringify(statistics), function () {
+        io.emit("send datas", statistics)
+        console.log("Statistics")
+
+    })
+}, 250)
+
+function Spring() {
+    clearInterval(i)
+    setInterval(game, 300)
+    weather = "spring";
+    io.sockets.emit('Spring', weather);
+}
+
+function Summer() {
+    clearInterval(i)
+    setInterval(game, 500)
+    weather = "summer";
+    io.sockets.emit('Summer', weather);
+}
+
+function Autumn() {
+    clearInterval(i)
+    setInterval(game, 500)
+    weather = "autumn";
+    io.sockets.emit('Autumn', weather);
+}
+
+function Winter() {
+    clearInterval(i)
+    setInterval(game, 1000)
+    weather = "winter";
+    io.sockets.emit('Winter', weather);
+}
